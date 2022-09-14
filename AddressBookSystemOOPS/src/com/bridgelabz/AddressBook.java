0fbe065735699dcd8abd1d6686024205f2f817b8
Pranali-22 <lpranali22@gmail.com>
2022-09-14 22:08:17 +0530

    Added : Address Book System UC2

--- AddressBookSystemOOPS/src/com/bridgelabz/AddressBook.java
+++ AddressBookSystemOOPS/src/com/bridgelabz/AddressBook.java
@@ -41,6 +41,7 @@ public class AddressBook {
 		System.out.println("Zip : "+zip);
 		System.out.println("Phone Number : "+phoneNumber);
 		System.out.println("Email Id : "+emailId);
+		
 	}
 	
 	public static void main(String[] args) {
@@ -59,7 +60,22 @@ public class AddressBook {
 		
 		AddressBook addressBookObj1 = new AddressBook(firstName1, lastName1, address1, city1, state1, zip1,
 				 phoneNo1, email1);
+		
+		System.out.println("\nEnter first name, last name, address, city, state, zip, phone number, email ID sequentially");
+		firstName1 = input.nextLine();
+	    lastName1 = input.nextLine();
+		address1 = input.nextLine();
+		city1 = input.nextLine();
+		state1 = input.nextLine();
+		zip1 = input.nextLine();
+		phoneNo1 = input.nextLine();
+		
+		AddressBook addressBookObj2 = new AddressBook(firstName1, lastName1, address1, city1, state1, zip1,
+				 phoneNo1, email1);
+		
 		addressBookObj1.displaData();
+		System.out.println();
+		addressBookObj2.displaData();
 		
 		input.close();
 
